735ffaa259cd87a56dfa4d0f0ff963e1f9fbc47d
Jayson Lee <jaysonlee@Jaysons-MacBook-Air.local>
2026-04-20 18:34:23 -0400

    Restrict /say to configured admin roles

--- commands/slash/say.js
+++ commands/slash/say.js
@@ -1,5 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 const Settings = require('../../models/settings');
+const { getConfiguredRoleIds, memberHasAnyConfiguredRole } = require('../../utils/roleHelpers');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -12,8 +13,25 @@ module.exports = {
     ),
 
   async execute(interaction) {
+    const settings = await Settings.findOne({ guildId: interaction.guild.id });
+    const adminRoleIds = getConfiguredRoleIds(settings?.adminRoleId);
+
+    if (adminRoleIds.length === 0) {
+      return interaction.reply({
+        content: 'No admin role IDs are configured yet. Please set `adminRoleId` first.',
+        flags: MessageFlags.Ephemeral
+      });
+    }
+
+    if (!memberHasAnyConfiguredRole(interaction.member, settings.adminRoleId)) {
+      return interaction.reply({
+        content: 'You do not have permission to use this command.',
+        flags: MessageFlags.Ephemeral
+      });
+    }
+
     const message = interaction.options.getString('message');
-    await interaction.reply({ content: 'Message sent!', ephemeral: true });
+    await interaction.reply({ content: 'Message sent!', flags: MessageFlags.Ephemeral });
     // Only send the message after replying, not as a second reply
     await interaction.channel.send(message);
   }
